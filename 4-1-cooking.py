65b1e1e68d140814a19abe980b5e1744eedab511
pongsagul <6310301003@cdti.ac.th>
2023-08-09 15:06:45 +0700

    commit1

--- 4-1-cooking.py
+++ 4-1-cooking.py
@@ -9,26 +9,36 @@ class Toast:
     pass
 
 def PourCoffee():
-    
+    print(f"{time.ctime()} - Pouring coffee")
+    return Coffee()
 
 async def ApplyButter():
-    
+    print(f"{time.ctime()} - Spreading butter on toast")
+    await asyncio.sleep(1)
     return
-  
+
 async def FryEggsAsync(howMany):
-    
+    print(f"{time.ctime()} - Heat pan to fry eggs")
+    await asyncio.sleep(3)
+    print(f"{time.ctime()} - Flying", howMany, "eggs")
+    await asyncio.sleep(3)
+    print(f"{time.ctime()} - Eggs are ready")
     return Egg()
 
 async def ToastAsync(slices):
     for slice in range(slices):
-      
+      print(f"{time.ctime()} - Toasting bread", slice + 1)
+      await asyncio.sleep(3)
+      print(f"{time.ctime()} - Bread", slice + 1, "toasted")
+      await ApplyButter()
+      print(f"{time.ctime()} - Toast", slice + 1, "ready")
     return Toast()
 
 async def main():
     cup = PourCoffee()
     print(f"{time.ctime()} - Coffee is ready")
-    await 
-    
+    await asyncio.gather(FryEggsAsync(10),ToastAsync(10))
+
 if __name__ == "__main__":
     import time
     s = time.perf_counter()
